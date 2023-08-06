6ba634bd8300053ece20b6fc28673406995b0527
Diego Larenas <111461360+larenas-d@users.noreply.github.com>
2023-08-06 16:20:45 -0400

    :bug: fix celery package

--- config/settings/base.py
+++ config/settings/base.py
@@ -67,7 +67,10 @@ CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 CELERY_WORKER_SEND_TASK_EVENTS = True
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std-setting-task_send_sent_event
 CELERY_TASK_SEND_SENT_EVENT = True
-broker_connection_retry_on_startup = True
+
+CELERY_TASK_ALWAYS_EAGER = True
+CELERY_TASK_EAGER_PROPAGATES = True
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 
 # DATABASES
 # ------------------------------------------------------------------------------
